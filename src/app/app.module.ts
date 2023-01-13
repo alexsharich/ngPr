1550b7e61c28570ad687436afd7879a7635e1f2d
alexsharich <alexsharich@gmail.com>
2023-01-13 23:44:13 +0100

    add testComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { appTestComponent } from './appTest/appTest.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    appTestComponent
   ],
   imports: [
     BrowserModule,
